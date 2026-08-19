20a9ac24060c941f3e43b3f4ab9d288f7ef771ec
RyuJungKyun <prank01@naver.com>
2026-08-19 13:07:22 +0900

    chore:프로젝트 초기 설정 및 의존성 추가

---
+++ src/main/java/com/meetmate/meetmatebackend/MeetmatebackendApplication.java
@@ -0,0 +1,13 @@
+package com.meetmate.meetmatebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetmatebackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetmatebackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meetmate/meetmatebackend/MeetmatebackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meetmate.meetmatebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetmatebackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
