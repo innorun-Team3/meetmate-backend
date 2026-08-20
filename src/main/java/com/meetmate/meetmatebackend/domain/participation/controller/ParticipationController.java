a10d12d3b3bb41a0b8b0b6779f8d3b175910600a
RyuJungKyun <prank01@naver.com>
2026-08-20 12:20:18 +0900

    feat: 도메인에 아키텍처 추가

---
+++ src/main/java/com/meetmate/meetmatebackend/domain/participation/controller/ParticipationController.java
@@ -0,0 +1,6 @@
+package com.meetmate.meetmatebackend.domain.participation.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ParticipationController {}
